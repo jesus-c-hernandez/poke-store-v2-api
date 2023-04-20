39ceed248e23cc007e8fce9ea94f6417dd811871
ivanrom-mm <ivan.romero@metricamovil.com>
2023-04-20 16:38:18 -0600

    BE Structure and server running

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// Define security middleware
+
+app.use(
+    // Add routes
+);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+// Using .env variables
+require('dotenv').config({ path: './.env' });
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { PORT: port } = process.env;
+
+app.use(express.static('public'));
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Using routes
+//app.use(require('./routes'));
+
+// Server running
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
